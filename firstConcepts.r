2b81ff4239e4f19a4430b8d71c31e22275033605
José Guilherme <guilherme-lima@hotmail.com>
2020-04-01 02:00:25 -0300

    training/test sets, accuracy, confusion matrix

---
+++ firstConcepts.r
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(caret)
+library(dslabs)
+data(heights)
+
+y <- heights$sex
+x <- heights$height
+
+#split dataset into training and test set
+set.seed(2007)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+# guess the outcome
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE)
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+
+#accuracy
+mean(y_hat == test_set$sex)
+
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+# examine the accuracy of 10 cutoffs
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+max(accuracy)
+best_cutoff <- cutoff[which.max(accuracy)]
+best_cutoff
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
+
+#check accuracy using confusion matrix
+table(predicted = y_hat, actual = test_set$sex)
+
+test_set %>% 
+  mutate(y_hat = y_hat) %>%
+  group_by(sex) %>% 
+  summarize(accuracy = mean(y_hat == sex))
+
+prev <- mean(y=="Male")  #prevalence indicating that males outnumbers females by a largin margin in the dataset
+prev
+
+library(e1071)
+confusionMatrix(data = y_hat, reference = test_set$sex)
+
+#low sensitivity because the prevalence of females is low, so databse is biased
+
